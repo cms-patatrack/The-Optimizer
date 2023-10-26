8a38799d7ee04129e46664accd41a65d9e0e825e
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2023-10-26 15:44:38 +0200

    Format and lint

--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -1,12 +1,10 @@
-from .fileManager import FileManager
+from .file_manager import FileManager
+
+
 class Optimizer:
-  
-  def __init__(self, file_manager) -> None:
-    self.file_manager = file_manager if file_manager is not None else FileManager()
-    pass
 
-  def optimize(self):
-    pass
+    def __init__(self, file_manager) -> None:
+        self.file_manager = file_manager if file_manager is not None else FileManager()
 
-      
-  
+    def optimize(self):
+        pass
