785dda9bd5a5050acd70f2f519537de8e675037c
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2024-03-04 14:30:42 +0100

    Handle different perameters types in initial position

--- tests/test_params.py
+++ tests/test_params.py
@@ -2,8 +2,8 @@ import optimizer
 
 lb1 = [0., 1, 0., 5.]
 lb2 = [0., 1, 0., "lower"]
-ub1 = [1, 2, 3, 4]
-ub2 = [2., 1, 1.]
+ub1 = [1, 2, 3, 6]
+ub2 = [2., 2, 1.]
 
 
 print([type(lb) for lb in lb1])
@@ -13,14 +13,24 @@ def always_true(x):
 
 objective = optimizer.Objective([always_true])
 
-pso = optimizer.MOPSO(objective=objective, lower_bounds=lb1, upper_bounds=ub1 )
+pso = optimizer.MOPSO(objective=objective, lower_bounds=lb1, upper_bounds=ub1, initial_particles_position='random')
 
 print([type(lb) for lb in pso.lower_bounds])
 print([type(ub) for ub in pso.upper_bounds])
 
-pso = optimizer.MOPSO(objective=objective, lower_bounds=lb1, upper_bounds=ub2 )
+pso = optimizer.MOPSO(objective=objective, lower_bounds=lb1, upper_bounds=ub2, initial_particles_position='random')
 
-pso = optimizer.MOPSO(objective=objective, lower_bounds=lb2, upper_bounds=ub1 )
-
+try:
+  pso = optimizer.MOPSO(objective=objective, lower_bounds=lb2, upper_bounds=ub1 )
+except ValueError as e:
+  print(e)
+  
 print([type(lb) for lb in pso.lower_bounds])
 print([type(ub) for ub in pso.upper_bounds])
+
+lbt = [0., 0, False]
+ubt = [1., 5, True]
+
+psot = optimizer.MOPSO(objective=objective, lower_bounds=lbt, upper_bounds=ubt, initial_particles_position='spread')
+
+print("\n".join([f"{p.position}" for p in psot.particles]))
