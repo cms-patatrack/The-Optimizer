5eef9520601c5297c6c8946ce48b51a6524a6895
Marco Picione <marcopicione98@gmail.com>
2024-07-12 11:18:28 +0200

    add test for reinforcement learning

--- optimizer/tester.py
+++ optimizer/tester.py
@@ -4,177 +4,154 @@ import matplotlib.pyplot as plt
 from pymoo.indicators.hv import HV
 import time
 
-def test(objective, num_agents, num_iterations, lower_bounds, upper_bounds, radius = None, plot = True):
+def test(objective, mopso_parameters, num_iterations, rl_model, ref_point, seeds, plot_paretos = True, print_results = True, known_pareto=None, verbose = 0):
+    results = {}
+    for seed in seeds:
+        result = test_seed(objective, mopso_parameters, num_iterations, rl_model, ref_point, seed, verbose = verbose)
+        results[f'{seed}'] = result
 
-    print("Starting MOPSO withot RL")
-    optimizer.Randomizer.rng = np.random.default_rng(43)
-    pso_no_rl = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
-                        rl_model=None, radius=radius)
+    if print_results: print_results(results)
+    if plot_paretos: plot_paretos(results, known_pareto)    
+    return results
 
-    start_time_no_rl = time.time()                    
-    pso_no_rl.optimize(num_iterations=num_iterations)
-    end_time_no_rl = time.time()
+def test_seed(objective, mopso_parameters, num_iterations, rl_model, ref_point, seed, verbose = 0):
 
-    print("Starting MOPSO with RL")
-    optimizer.Randomizer.rng = np.random.default_rng(43)
-    pso_rl = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
-                        rl_model="./models/masked_model", radius=radius)
+    if verbose > 0 : print(f"SEED {seed}")
 
-    start_time_rl = time.time() 
-    pso_rl.optimize(num_iterations=num_iterations)
-    end_time_rl = time.time() 
+    #Optimizers
+    optimizers = []
 
-    if plot:
-        plt.figure()
-        pareto_x_no_rl = [particle.fitness[0] for particle in pso_no_rl.pareto_front]
-        pareto_y_no_rl = [particle.fitness[1] for particle in pso_no_rl.pareto_front]
-        pareto_x_rl = [particle.fitness[0] for particle in pso_rl.pareto_front]
-        pareto_y_rl = [particle.fitness[1] for particle in pso_rl.pareto_front]
-        plt.scatter(pareto_x_no_rl, pareto_y_no_rl, s=5, c='red', label = "MOPSO")
-        plt.scatter(pareto_x_rl, pareto_y_rl, s=5, label = "Reinforcement Learning")
-        plt.xlabel("Objective 1")
-        plt.ylabel("Objective 2")
-        plt.legend()
-        plt.savefig("pareto_comparison.png")
-        plt.close()
-    print("Evaluations ", pso_rl.evaluations / (num_agents * num_iterations) * 100, "%")
-    ref_point = [2,2]
+    if verbose > 1 : print("Starting MOPSO withot RL")
+    optimizer.Randomizer.rng = np.random.default_rng(seed)
+    pso = optimizer.MOPSO(objective=objective, lower_bounds=mopso_parameters['lower_bounds'], upper_bounds=mopso_parameters['upper_bounds'],
+                                num_particles=mopso_parameters['num_particles'],
+                                inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
+                                rl_model=None, radius=mopso_parameters['radius'])
+
+    start_time_pso = time.time()                    
+    pso.optimize(num_iterations=num_iterations)
+    end_time_pso = time.time()
+    optimizers.append(pso)
+
+    if verbose > 1 : print("Starting MOPSO with trained policy")
+    optimizer.Randomizer.rng = np.random.default_rng(seed)
+    pso_trained_policy = optimizer.MOPSO(objective=objective, lower_bounds=mopso_parameters['lower_bounds'], upper_bounds=mopso_parameters['upper_bounds'],
+                                num_particles=mopso_parameters['num_particles'],
+                                inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
+                                rl_model=rl_model, radius=mopso_parameters['radius'])
+
+    start_time_pso_trained_policy = time.time() 
+    pso_trained_policy.optimize(num_iterations=num_iterations)
+    end_time_pso_trained_policy = time.time() 
+    optimizers.append(pso_trained_policy)
+
+    if verbose > 1 : print("Starting MOPSO with random policy")
+    optimizer.Randomizer.rng = np.random.default_rng(seed)
+    pso_random_policy = optimizer.MOPSO(objective=objective, lower_bounds=mopso_parameters['lower_bounds'], upper_bounds=mopso_parameters['upper_bounds'],
+                                num_particles=mopso_parameters['num_particles'],
+                                inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
+                                rl_model='random', radius=mopso_parameters['radius'])
+
+    start_time_pso_random_policy = time.time() 
+    pso_random_policy.optimize(num_iterations=num_iterations)
+    end_time_pso_random_policy = time.time()
+    optimizers.append(pso_random_policy)
+
+    # Results
+    evaluations_pso = pso.evaluations
+    evaluations_pso_trained_policy = pso_trained_policy.evaluations
+    evaluations_pso_random_policy = pso_random_policy.evaluations
+
+    pareto_pso = [p.fitness for p in pso.pareto_front]
+    pareto_pso_trained_policy = [p.fitness for p in pso_trained_policy.pareto_front]
+    pareto_pso_random_policy = [p.fitness for p in pso_random_policy.pareto_front]
+
     ind = HV(ref_point=ref_point)
-    print("Pareto points ", ind(np.array([p.fitness for p in pso_rl.pareto_front])) / ind(np.array([p.fitness for p in pso_no_rl.pareto_front])) * 100, "%")
-    delta_rl = end_time_rl - start_time_rl
-    delta_no_rl = end_time_no_rl - start_time_no_rl
-    time_diff = (delta_no_rl - delta_rl) / delta_no_rl
-    print("Time saved ", time_diff * 100, "%")
-    return time_diff
+    hv_pso = ind(np.array(pareto_pso))
+    hv_pso_trained_policy= ind(np.array(pareto_pso_trained_policy))
+    hv_pso_random_policy= ind(np.array(pareto_pso_random_policy))
 
-def test_time(objective, num_agents, num_iterations, lower_bounds, upper_bounds, times):
-    time_decreases = []
+    time_pso = start_time_pso - end_time_pso
+    time_pso_trained_policy = start_time_pso_trained_policy - end_time_pso_trained_policy
+    time_random_policy = start_time_pso_random_policy - end_time_pso_random_policy
 
-    for t in times:
-        objective.sleep_time = t
-        time_diff = test(objective, num_agents, num_iterations, lower_bounds, upper_bounds, plot = False)
-        time_decreases.append(time_diff * 100)
+    result = {'seed'   : seed,
+              'models' : {'pso':                {'evaluations'  : evaluations_pso,
+                                                 'pareto_front' : pareto_pso,
+                                                 'hyper_volume' : hv_pso,
+                                                 'time'         : time_pso,
+                                                },
 
-    np.save("times.npy", time_decreases)
+                          'pso_trained_policy': {'evaluations'  : evaluations_pso_trained_policy,
+                                                 'pareto_front' : pareto_pso_trained_policy,
+                                                 'hyper_volume' : hv_pso_trained_policy,
+                                                 'time'         : time_pso_trained_policy
+                                                },
+
+                          'pso_random_policy':  {'evaluations'  : evaluations_pso_random_policy,
+                                                 'pareto_front' : pareto_pso_random_policy,
+                                                 'hyper_volume' : hv_pso_random_policy,
+                                                 'time'         : time_random_policy
+                                                }
+                        }
+              }
+    
+    return result
+
+def print_results(results):
+    models = results[next(iter(results))]['models']
+    models_keys = models.keys()
+    metrics_keys = list(models[next(iter(models))].keys()).remove('pareto_front')
+
+    num_metrics = len(metrics_keys)
+    num_models = len(models_keys)
+    num_seeds = len(results.keys())
+
+    metrics = np.zeros((num_models, num_metrics))
+
+    for s, seed in enumerate(results.keys()):
+        for i, mod in enumerate(models_keys):
+            for j, met in enumerate(metrics_keys):
+                metrics[s][i][j] = results[seed]['models'][mod][met]
+
+    means = np.mean(metrics, axis = 1)
+    stds = np.std(metrics, axis = 1)
+
+    for i, mod in enumerate(models_keys):
+        print(f"Model {mod}:")
+        for j, met in enumerate(metrics_keys):
+            print(f"\t{met}: {round(means[i][j], 2)} +- {round(stds[i][j] / np.sqrt(num_seeds), 2)}")
+
+def plot_paretos(results, known_pareto = None):
+    for r in results.keys():
+        result = results[r]
+        paretos = {}
+        for mod in result['models'].keys():
+            model = result['models'][mod]
+            pareto = model['pareto_front']
+            axs = []
+            num_objectives = len(pareto[0])
+            for obj in range(num_objectives):
+                axs.append([fitness[obj] for fitness in pareto])
+            paretos[mod] = axs
+        if num_objectives == 2: plot_pareto_2d(paretos, result['seed'], known_pareto)
+        elif num_objectives == 3: plot_pareto_3d(paretos, result['seed'], known_pareto)
+        else: print(f"No implementation of plot fuction for {num_objectives} objectives")
+
+def plot_pareto_2d(paretos, seed, known_pareto = None):
     plt.figure()
-    plt.plot(times, time_decreases)
-    plt.xlabel("Sleep time [s]")
-    plt.ylabel("Time decrease [%]")
-    plt.savefig("time_decrease.png")
+    if known_pareto is not None:
+        plt.scatter(known_pareto[0], known_pareto[1], c='red', s=5, label = mod)
+    for mod in paretos.keys():
+        pareto = paretos[mod] 
+        plt.scatter(pareto[0], pareto[1], s=5, label = mod)
+
+    plt.xlabel("Objective 1")
+    plt.ylabel("Objective 2")
+    plt.title(f"Seed: {seed}")
+    plt.legend()
+    plt.savefig(f"Pareto_front_seed_{seed}.png")
     plt.close()
 
-def test_random(model, objective, num_agents, num_iterations, lower_bounds, upper_bounds, radius=None, plot = True, max_time = np.inf):
-    seed = 40
-    # OPTIMAL MOPSO
-    print("Starting Optimal MOPSO ")
-    optimizer.Randomizer.rng = np.random.default_rng(seed)
-    pso_optimal = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents * 2,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True)
-    start_time_optimal = time.time()                    
-    pso_optimal.optimize(num_iterations=num_iterations * 2, max_time = max_time * 2)
-    end_time_optimal = time.time()
-    
-    # MOPSO
-    print("Starting MOPSO ")
-    optimizer.Randomizer.rng = np.random.default_rng(seed)
-    pso_no_rl = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True)
-    start_time_no_rl = time.time()                    
-    pso_no_rl.optimize(num_iterations=num_iterations, max_time = max_time)
-    end_time_no_rl = time.time()
-    
-    # MOPSO WITH RANDOM POLICY
-    print("Starting MOPSO with random policy ")
-    optimizer.Randomizer.rng = np.random.default_rng(seed)
-    pso_rl_random = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
-                        rl_model="Random", radius=radius)
-
-    start_time_rl_random = time.time()                    
-    pso_rl_random.optimize(num_iterations=num_iterations, max_time = max_time)
-    end_time_rl_random = time.time()
-
-    # MOPSO WITH TRAINED POLICY
-    print("Starting MOPSO with trained policy")
-    optimizer.Randomizer.rng = np.random.default_rng(seed)
-    pso_rl = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
-                        rl_model=model, radius=radius)
-
-    start_time_rl = time.time() 
-    pso_rl.optimize(num_iterations=num_iterations, max_time = max_time)
-    end_time_rl = time.time()
-
-    if plot:
-        alpha = 0.7
-        pareto_x_optimal = [particle.fitness[0] for particle in pso_optimal.pareto_front]
-        pareto_y_optimal = [particle.fitness[1] for particle in pso_optimal.pareto_front]
-        pareto_x_no_rl = [particle.fitness[0] for particle in pso_no_rl.pareto_front]
-        pareto_y_no_rl = [particle.fitness[1] for particle in pso_no_rl.pareto_front]
-        pareto_x_rl = [particle.fitness[0] for particle in pso_rl.pareto_front]
-        pareto_y_rl = [particle.fitness[1] for particle in pso_rl.pareto_front]
-        pareto_x_rl_random = [particle.fitness[0] for particle in pso_rl_random.pareto_front]
-        pareto_y_rl_random = [particle.fitness[1] for particle in pso_rl_random.pareto_front]
-        plt.figure()
-        plt.scatter(pareto_x_optimal, pareto_y_optimal, s=5, c='red', label = "Optimal MOPSO", alpha = alpha)
-        plt.scatter(pareto_x_no_rl, pareto_y_no_rl, s=5, c='blue', label = "MOPSO", alpha = alpha)
-        plt.scatter(pareto_x_rl, pareto_y_rl, s=5, c='green', label = "Trained policy", alpha = alpha)
-        plt.scatter(pareto_x_rl_random, pareto_y_rl_random, s=5, c='orange', label = "Random policy", alpha = alpha)
-        plt.xlabel("Objective 1")
-        plt.ylabel("Objective 2")
-        plt.legend()
-        plt.savefig("pareto_comparison_policy_max_time" + str(max_time) + "_seed_" + str(seed) + ".png")
-        plt.close()
-    print("Evaluations Trained / MOPSO", np.sum(pso_rl.evaluations) / (num_agents * num_iterations) * 100, "%")
-    print("Evaluations Random / MOPSO", np.sum(pso_rl_random.evaluations) / (num_agents * num_iterations) * 100, "%")
-    print("Evaluations Trained / Random", np.sum(pso_rl.evaluations) / np.sum(pso_rl_random.evaluations) * 100, "%")
-    ref_point = [2,2]
-    ind = HV(ref_point=ref_point)
-    print("HV Trained / MOPSO", ind(np.array([p.fitness for p in pso_rl.pareto_front])) / ind(np.array([p.fitness for p in pso_no_rl.pareto_front])) * 100, "%")
-    print("HV Random / MOPSO", ind(np.array([p.fitness for p in pso_rl_random.pareto_front])) / ind(np.array([p.fitness for p in pso_no_rl.pareto_front])) * 100, "%")
-    print("HV Trained / Random", ind(np.array([p.fitness for p in pso_rl.pareto_front])) / ind(np.array([p.fitness for p in pso_rl_random.pareto_front])) * 100, "%")
-    
-    delta_rl = end_time_rl - start_time_rl
-    delta_no_rl = end_time_no_rl - start_time_no_rl
-    time_diff = (delta_no_rl - delta_rl) / delta_no_rl
-    # print("Time decrease ", time_diff * 100, "%")
-    return time_diff
-
-def test_num_evaluations(objective, num_agents, num_iterations, lower_bounds, upper_bounds, plot_index, radius = None,):
-    print("Starting MOPSO with RL")
-    optimizer.Randomizer.rng = np.random.default_rng(43)
-    pso_rl = optimizer.MOPSO(objective=objective, lower_bounds=lower_bounds, upper_bounds=upper_bounds,
-                        num_particles=num_agents,
-                        inertia_weight=0.6, cognitive_coefficient=1, social_coefficient=2, initial_particles_position='random', incremental_pareto=True, 
-                        rl_model="./models/masked_model", radius=radius)
-    pso_rl.optimize(num_iterations=num_iterations)
-    evaluations = pso_rl.evaluations
-    plt.figure()
-    plt.hist(evaluations, bins = list(range(num_agents)))
-    plt.xticks(list(range(0, num_agents + 1, 5)), ha='center')
-    plt.xlabel("Evaluations per iteration")
-    plt.ylabel("Counts")
-    plt.savefig("./plots/evaluation_histogram_" + str(plot_index) + ".png")
-    plt.close()
-    plt.figure()
-    pareto_x_rl = [particle.fitness[0] for particle in pso_rl.pareto_front]
-    pareto_y_rl = [particle.fitness[1] for particle in pso_rl.pareto_front]
-    plt.scatter(pareto_x_rl, pareto_y_rl, s=5, c='red', label = "Trained policy")
-    plt.savefig("./plots/evaluation_pareto_" + str(plot_index) + ".png")
-    plt.close()
-    print("Tot evaluations: ", np.sum(evaluations) / (num_agents * num_iterations) * 100)
-    ref_point = [5, 5]
-    ind = HV(ref_point=ref_point)
-    hv = ind(np.array([p.fitness for p in pso_rl.pareto_front]))
-    return pso_rl.evaluations,pso_rl.bad_points_per_iteration, pso_rl.pareto_points_per_iteration, hv
-
-
-    
+def plot_pareto_3d(paretos, known_pareto = None):
+    pass    
